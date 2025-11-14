0add3a31be2c2088be4e5f86eae4f26aa2028142
烈霖 <lielin.hyl@alibaba-inc.com>
2025-11-14 12:02:08 +0800

    * fix test case for env manager

--- tests/run.py
+++ tests/run.py
@@ -5,49 +5,58 @@
 # is as follows:
 # --------------------------------------------------------
 # Copyright (c) Alibaba, Inc. and its affiliates
+# flake8: noqa: E402
 
 import argparse
 import os
 import sys
 import unittest
+
 import coverage
 
 # start the coverage immediately
-cov = coverage.Coverage(include='data_juicer/**')
+cov = coverage.Coverage(include="data_juicer/**")
 cov.start()
 
+from data_juicer.utils.unittest_utils import (
+    get_partial_test_cases,
+    set_clear_model_flag,
+    set_from_fork_flag,
+)
 from loguru import logger
 
-from data_juicer.utils.unittest_utils import set_clear_model_flag, get_partial_test_cases, set_from_fork_flag
-
-file_dir = os.path.join(os.path.dirname(__file__), '..')
+file_dir = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(file_dir)
 
-parser = argparse.ArgumentParser('test runner')
-parser.add_argument('--tag', choices=["standalone", "ray"],
-                    default="standalone",
-                    help="the tag of tests being run")
-parser.add_argument('--pattern', default='test_*.py', help='test file pattern')
-parser.add_argument('--mode', default='partial',
-                    help='test mode. Should be one of the ["partial", '
-                         '"regression"]. "partial" means only test on the '
-                         'unit tests of the changed files. "regression" means '
-                         'test on all unit tests.')
-parser.add_argument('--from-fork', default=False, type=bool,
-                    help='Whether this unittest is conducted on a forked repo '
-                         'instead of a branch in the Data-Juicer repo.')
-parser.add_argument('--test_dir',
-                    default='tests',
-                    help='directory to be tested')
-parser.add_argument('--clear_model',
-                    default=False,
-                    type=bool,
-                    help='whether to clear the downloaded models for tests. '
-                         'It\'s False in default.')
+parser = argparse.ArgumentParser("test runner")
+parser.add_argument("--tag", choices=["standalone", "ray"], default="standalone", help="the tag of tests being run")
+parser.add_argument("--pattern", default="test_*.py", help="test file pattern")
+parser.add_argument(
+    "--mode",
+    default="partial",
+    help='test mode. Should be one of the ["partial", '
+    '"regression"]. "partial" means only test on the '
+    'unit tests of the changed files. "regression" means '
+    "test on all unit tests.",
+)
+parser.add_argument(
+    "--from-fork",
+    default=False,
+    type=bool,
+    help="Whether this unittest is conducted on a forked repo " "instead of a branch in the Data-Juicer repo.",
+)
+parser.add_argument("--test_dir", default="tests", help="directory to be tested")
+parser.add_argument(
+    "--clear_model",
+    default=False,
+    type=bool,
+    help="whether to clear the downloaded models for tests. " "It's False in default.",
+)
 args = parser.parse_args()
 
 set_clear_model_flag(args.clear_model)
 set_from_fork_flag(args.from_fork)
+
 
 class TaggedTestLoader(unittest.TestLoader):
     def __init__(self, tag="standalone", included_test_files=None):
@@ -56,16 +65,16 @@ class TaggedTestLoader(unittest.TestLoader):
         if isinstance(included_test_files, str):
             included_test_files = [included_test_files]
         self.included_test_files = included_test_files
-    
+
     def loadTestsFromTestCase(self, testCaseClass):
         # set tag to testcase class
-        setattr(testCaseClass, 'current_tag', self.tag)
+        setattr(testCaseClass, "current_tag", self.tag)
         test_names = self.getTestCaseNames(testCaseClass)
         loaded_suite = self.suiteClass()
         for test_name in test_names:
             test_case = testCaseClass(test_name)
             test_method = getattr(test_case, test_name)
-            if self.tag in getattr(test_method, '__test_tags__', ["standalone"]):
+            if self.tag in getattr(test_method, "__test_tags__", ["standalone"]):
                 loaded_suite.addTest(test_case)
         return loaded_suite
 
@@ -80,20 +89,20 @@ class TaggedTestLoader(unittest.TestLoader):
         else:
             return match
 
-def gather_test_cases(test_dir, pattern, tag, mode='partial'):
+
+def gather_test_cases(test_dir, pattern, tag, mode="partial"):
     test_to_run = unittest.TestSuite()
-    partial_test_files = get_partial_test_cases() if mode == 'partial' else None
+    partial_test_files = get_partial_test_cases() if mode == "partial" else None
     test_loader = TaggedTestLoader(tag, included_test_files=partial_test_files)
     discover = test_loader.discover(test_dir, pattern=pattern, top_level_dir=None)
     for suite_discovered in discover:
-        print('suite_discovered', suite_discovered)
+        print("suite_discovered", suite_discovered)
         for test_suite in suite_discovered:
-            print('test_suite', test_suite)
+            print("test_suite", test_suite)
             if isinstance(test_suite, unittest.loader._FailedTest):
                 raise test_suite._exception
             for test_case in test_suite:
-                logger.info(f'Add test case [{test_case._testMethodName}]'
-                            f' from {test_case.__class__.__name__}')
+                logger.info(f"Add test case [{test_case._testMethodName}]" f" from {test_case.__class__.__name__}")
                 test_to_run.addTest(test_case)
     return test_to_run
 
@@ -101,9 +110,8 @@ def gather_test_cases(test_dir, pattern, tag, mode='partial'):
 def main():
     global cov
     runner = unittest.TextTestRunner()
-    test_suite = gather_test_cases(os.path.abspath(args.test_dir),
-                                   args.pattern, args.tag, args.mode)
-    logger.info(f'There are {len(test_suite._tests)} test cases to run.')
+    test_suite = gather_test_cases(os.path.abspath(args.test_dir), args.pattern, args.tag, args.mode)
+    logger.info(f"There are {len(test_suite._tests)} test cases to run.")
     res = runner.run(test_suite)
 
     cov.stop()
@@ -116,13 +124,13 @@ def main():
     try:
         cov.report(ignore_errors=True)
     except Exception as e:
-        logger.error(f'Failed to print coverage report: {e}')
+        logger.error(f"Failed to print coverage report: {e}")
 
     try:
-        cov.html_report(directory=f'coverage_report_{args.tag}', ignore_errors=True)
+        cov.html_report(directory=f"coverage_report_{args.tag}", ignore_errors=True)
     except Exception as e:
-        logger.error(f'Failed to generate coverage report in html: {e}')
+        logger.error(f"Failed to generate coverage report in html: {e}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_env_manager.py
+++ tests/test_env_manager.py
@@ -2,8 +2,10 @@ import os
 import tempfile
 import unittest
 from unittest.mock import patch
-from data_juicer_sandbox.env_manager import CondaEnv, VirtualEnv
+
 from data_juicer.utils.unittest_utils import DataJuicerTestCaseBase
+
+from data_juicer_sandbox.env_manager import CondaEnv, VirtualEnv
 
 
 class TestCondaEnv(DataJuicerTestCaseBase):
@@ -13,10 +15,10 @@ class TestCondaEnv(DataJuicerTestCaseBase):
         self.test_dir = tempfile.TemporaryDirectory()
         self.env_name = "test_conda_env"
         self.config_path = os.path.join(self.test_dir.name, "env_config.yaml")
-        with open(self.config_path, 'w') as f:
+        with open(self.config_path, "w") as f:
             f.write(f"name: {self.env_name}\n")
         self.requirements_file_path = os.path.join(self.test_dir.name, "requirements.txt")
-        with open(self.requirements_file_path, 'w') as f:
+        with open(self.requirements_file_path, "w") as f:
             f.write("numpy\npandas\n")
         self.lib_home = self.test_dir.name
 
@@ -40,8 +42,9 @@ class TestCondaEnv(DataJuicerTestCaseBase):
         with self.assertRaises(ValueError):
             CondaEnv(env_name="dummy").check_availability()
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_create_env_but_exists(self, mock_run):
+    def test_create_env_but_exists(self, mock_run, mock_which):
         mock_run.return_value.stdout = '{"envs": ["/mock/envs/%s"]}' % self.env_name
         env = CondaEnv(env_name=self.env_name)
         self.assertTrue(env.exists())
@@ -51,82 +54,93 @@ class TestCondaEnv(DataJuicerTestCaseBase):
         # called in exists again and no new calling on subprocess.run to create env
         self.assertEqual(mock_run.call_count, 2)
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_create_new_env_from_name(self, mock_run):
+    def test_create_new_env_from_name(self, mock_run, mock_which):
         mock_run.return_value.returncode = 0
         env = CondaEnv(env_name=self.env_name, env_py_version="3.9")
         env.create()
         # called in exists and called once when creating this new env
         self.assertEqual(mock_run.call_count, 2)
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_create_new_env_from_config_path(self, mock_run):
+    def test_create_new_env_from_config_path(self, mock_run, mock_which):
         mock_run.return_value.returncode = 0
         env = CondaEnv(env_config_path=self.config_path)
         env.create()
         # called in exists and called once when creating this new env
         self.assertEqual(mock_run.call_count, 2)
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_create_new_env_failed(self, mock_run):
+    def test_create_new_env_failed(self, mock_run, mock_which):
         mock_run.return_value.returncode = 1
         with self.assertRaises(ValueError):
             env = CondaEnv(env_config_path=self.config_path)
             env.create()
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_exists_false(self, mock_run):
+    def test_exists_false(self, mock_run, mock_which):
         mock_run.return_value.stdout = '{"envs": []}'
         env = CondaEnv(env_name=self.env_name)
         self.assertFalse(env.exists())
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_install_deps_from_requirements_file(self, mock_run):
+    def test_install_deps_from_requirements_file(self, mock_run, mock_which):
         mock_run.return_value.returncode = 0
         env = CondaEnv(env_name=self.env_name)
         env.install_py_deps(self.requirements_file_path)
         self.assertTrue(mock_run.called)
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_install_deps_from_lib_home(self, mock_run):
+    def test_install_deps_from_lib_home(self, mock_run, mock_which):
         mock_run.return_value.returncode = 0
         env = CondaEnv(env_name=self.env_name)
         env.install_py_deps(self.lib_home)
         self.assertTrue(mock_run.called)
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_install_deps_from_non_existing_path(self, mock_run):
+    def test_install_deps_from_non_existing_path(self, mock_run, mock_which):
         with self.assertRaises(FileNotFoundError):
             mock_run.return_value.returncode = 0
             env = CondaEnv(env_name=self.env_name)
             env.install_py_deps("/non/existing/path")
         self.assertFalse(mock_run.called)
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_install_deps_from_list(self, mock_run):
+    def test_install_deps_from_list(self, mock_run, mock_which):
         mock_run.return_value.returncode = 0
         env = CondaEnv(env_name=self.env_name)
         env.install_py_deps(["numpy", "pandas"])
         self.assertTrue(mock_run.called)
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_install_deps_invalid(self, mock_run):
+    def test_install_deps_invalid(self, mock_run, mock_which):
         with self.assertRaises(TypeError):
             mock_run.return_value.returncode = 0
             env = CondaEnv(env_name=self.env_name)
             env.install_py_deps({"invalid_input": "invalid_val"})
         self.assertFalse(mock_run.called)
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_install_deps_failed(self, mock_run):
+    def test_install_deps_failed(self, mock_run, mock_which):
         with self.assertRaises(RuntimeError):
             mock_run.return_value.returncode = 1
             env = CondaEnv(env_name=self.env_name)
             env.install_py_deps(["numpy", "pandas"])
         self.assertTrue(mock_run.called)
 
+    @patch("data_juicer_sandbox.env_manager.shutil.which", return_value="/usr/bin/conda")
     @patch("data_juicer_sandbox.env_manager.subprocess.run")
-    def test_run_cmd(self, mock_run):
+    def test_run_cmd(self, mock_run, mock_which):
         mock_run.return_value.returncode = 0
         env = CondaEnv(env_name=self.env_name)
         env.run_cmd("echo hello")
@@ -137,10 +151,10 @@ class TestVirtualEnv(DataJuicerTestCaseBase):
     def setUp(self):
         super().setUp()
 
-        self.env_name = 'test_virtual_env'
+        self.env_name = "test_virtual_env"
         self.test_dir = tempfile.TemporaryDirectory()
         self.requirements_file_path = os.path.join(self.test_dir.name, "requirements.txt")
-        with open(self.requirements_file_path, 'w') as f:
+        with open(self.requirements_file_path, "w") as f:
             f.write("numpy\npandas\n")
         self.lib_home = self.test_dir.name
 
@@ -217,5 +231,5 @@ class TestVirtualEnv(DataJuicerTestCaseBase):
         self.assertEqual(mock_run.call_count, 2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
